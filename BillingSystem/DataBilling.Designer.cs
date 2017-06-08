9349f47b0d1689cc1ceb812698cb17c6bc2a4ae2
rossycaraballo <rossy.esther.caraballo@gmail.com>
2017-06-08 16:07:13 -0700

    asdfadsf

--- BillingSystem/DataBilling.Designer.cs
+++ BillingSystem/DataBilling.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Brayan Mota\Source\Repos\Billing-System\BillingSystem\DataBilling.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\bmota\Source\BillingSys\Billing-System\BillingSystem\DataBilling.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
